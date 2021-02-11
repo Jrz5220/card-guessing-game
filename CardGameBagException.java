1f66cf928d4a9322451320258445838bf2c0dc8e
Jrz5220 <53059217+Jrz5220@users.noreply.github.com>
2021-02-10 20:35:25 -0500

    Add files via upload
    java files upload

---
+++ CardGameBagException.java
@@ -0,0 +1,18 @@
+package lab2;
+
+public class CardGameBagException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public CardGameBagException() {
+		this("Bag must contain less items than the card list.");
+	}
+	
+	public CardGameBagException(String message) {
+		super(message);
+	}
+
+}
