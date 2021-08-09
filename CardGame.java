61ee92030fc3a31eb15e4bac137f665848278dde
Jrz5220 <53059217+Jrz5220@users.noreply.github.com>
2021-08-09 13:36:05 -0400

    Update game instructions in main

--- CardGame.java
+++ CardGame.java
@@ -3,8 +3,8 @@ public class CardGame {
 	public static void main(String[] args) {
 		
 		System.out.println("Six cards from the following list will be put into a bag randomly: \nA, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K.");
-		System.out.println("Two players will take turns guessing which cards are in the bag.");
-		System.out.println("The first player to correctly guess all the cards wins.");
+		System.out.println("Two players will take turns guessing which cards were put into the bag.");
+		System.out.println("The first player to correctly guess all six cards wins.");
 		
 		GuessCardGame newGame = new GuessCardGame();
 		newGame.startGame();
